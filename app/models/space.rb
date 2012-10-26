19af9f6f2be38d2a26a2feff08483d9cc42fb907
Alexander Lang <alex@upstre.am>
2012-10-26 12:40:55 +0200

    adds dependent destroy

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,7 +1,7 @@
 class Space < ActiveRecord::Base
   before_create :set_name
-  has_many :categories
-  has_many :members, order: 'name'
+  has_many :categories, dependent: :destroy
+  has_many :members, order: 'name', dependent: :destroy
   has_many :resources, through: :categories, order: 'name'
   serialize :admins
 
