ffe1d55e1ab0673aef8ece722bdeed5fc77d9422
Alexander Lang <alex@upstre.am>
2014-10-15 12:26:24 +0200

    removes secret token from code

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OfficeSpace::Application.config.secret_token = '1dbcfba9beefa645cddf989dfd20c06f939ee448a88257dd527ad473d8f01c48d522000c9d5b4b2bd366c2ed8a92ae8ca51e6d266937a382472c09373f5c7ec4'
+OfficeSpace::Application.config.secret_token = ENV['SECRET_TOKEN']
