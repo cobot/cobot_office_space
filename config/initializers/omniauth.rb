22ff9da8a1682309c0d69394b1807d0ccf312acf
Alexander Lang <alex@cobot.me>
2022-11-03 10:37:12 +0100

    add using space subdomain for oauth so that users on spaces with a custom domain don't have to log in again when acceesing this app in in iframe.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,5 @@ require "omniauth/strategies/cobot"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cobot, ENV['COBOT_CLIENT_ID'], ENV['COBOT_CLIENT_SECRET'],
-    scope: 'read navigation', client_options: {
-      site: Rails.application.config.cobot_site,
-      authorize_url: "#{Rails.application.config.cobot_site}/oauth/authorize",
-      token_url: "#{Rails.application.config.cobot_site}/oauth/access_token"
-    }
+    scope: 'read navigation'
 end
